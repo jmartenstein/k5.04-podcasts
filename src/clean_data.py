966a02884f1f4d75e6e3ff47c01c9c42a84942ce
Justin Martenstein <jmartenstein@knockcrm.com>
2025-04-12 12:38:08 -0400

    add imputers

--- src/clean_data.py
+++ src/clean_data.py
@@ -1,7 +1,11 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+
+from sklearn.experimental import enable_iterative_imputer
+
 import sklearn.preprocessing as pre
+import sklearn.impute as imp
 
 import argparse
 
@@ -46,10 +50,20 @@ df_test["Listening_Time_minutes"] = np.nan
 df_ = pd.concat([df_train, df_test])
 #df_.info()
 
-df_["Length_Clean" ] = df_train[ "Episode_Length_minutes" ].apply( lambda v: 120.5 if v > 120.5 else v )
+df_["Length_Clean" ] = df_[ "Episode_Length_minutes" ].apply( lambda v: 120.5 if v > 120.5 else v )
 df_["IsGuest"] = df_["Guest_Popularity_percentage"].apply( lambda v: 0 if np.isnan(v) else 1 )
 df_["Num_Ads"] = df_["Number_of_Ads"].fillna(0)
 df_["Name_And_Episode"] = df_["Podcast_Name"] + " " + df_["Episode_Title"]
+
+df_["Episode_Completion"] = df_["Listening_Time_minutes"] / df_["Length_Clean"]
+df_['Episode_Completion'] = df_['Episode_Completion'].apply( lambda v: 1 if v > 1 else v )
+
+median_impute = imp.SimpleImputer(missing_values=np.nan, strategy='median', add_indicator=True)
+median_impute.fit(df_[['Length_Clean']])
+length_features = median_impute.transform(df_[['Length_Clean']])
+
+df_['Length_Simple_Impute'] = length_features[:,0]
+df_['Length_Missing'] = length_features[:,1]
 
 hist, bin_edges = np.histogram( df_["Num_Ads"],
                                 bins=[ df_["Num_Ads"].min(),
@@ -77,6 +91,11 @@ df_train_clean["Ads_Bin_Encoded"] = adsbin_enc.transform(df_train_clean[["Num_Ad
 name_enc.fit( df_train_clean[["Name_And_Episode"]], df_train_clean[TARGET_COLUMN] )
 df_train_clean["Name_And_Episode_Encoded"] = name_enc.transform(df_train_clean[["Name_And_Episode"]])
 
+#iter_impute = imp.IterativeImputer(missing_values=np.nan, max_iter=20)
+#impute_cols = ["Length_Clean", "Completion_Simple_Impute", "Host_Popularity_percentage",
+#               "Num_Ads_Bin", "Ads_Bin_Encoded", "Genre_Encoded", "Name_And_Episode_Encoded"]
+#iter_impute.fit(df_train_clean[impute_cols])
+#df_train_clean["Length_Iter_Impute" ] = iter_impute.transform(df_train_clean[impute_cols])[:,0]
 
 # prep the cleaned test data to write
 df_test_clean = df_[ df_[ TARGET_COLUMN ].isna() ].copy()
@@ -85,6 +104,9 @@ df_test_clean.drop( TARGET_COLUMN, axis=1, inplace=True )
 df_test_clean["Genre_Encoded"] = genre_enc.transform(df_test_clean[["Genre"]])
 df_test_clean["Ads_Bin_Encoded"] = adsbin_enc.transform(df_test_clean[["Num_Ads_Bin"]])
 df_test_clean["Name_And_Episode_Encoded"] = name_enc.transform(df_test_clean[["Name_And_Episode"]])
+
+#iter_impute.fit(df_test_clean[impute_cols])
+#df_test_clean["Length_Iter_Impute" ] = iter_impute.transform(df_test_clean[impute_cols])[:,0]
 
 if args["write"]:
 
