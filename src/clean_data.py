96afdf2e93293f310d2054cd415369d0db82dad6
Justin Martenstein <jmartenstein@knockcrm.com>
2025-04-13 15:50:17 -0400

    Generate target encoded features; may need nested cross validation?

--- src/clean_data.py
+++ src/clean_data.py
@@ -16,8 +16,8 @@ from datetime import datetime
 DATA_DIR = "../data/kaggle"
 ORIG_DIR = "../data/original"
 
-TARGET_COLUMN = "Listening_Time_minutes"
-
+TARGET1 = "Listening_Time_minutes"
+TARGET2 = "Episode_Completion_percentage"
 
 ### FUNCTIONS ###
 
@@ -45,25 +45,27 @@ df_test  = pd.read_csv(f"{DATA_DIR}/test.csv")
 df_orig  = pd.read_csv(f"{ORIG_DIR}/podcast_dataset.csv")
 
 # add the target column for the test data
-df_test["Listening_Time_minutes"] = np.nan
+df_test[TARGET1] = np.nan
 
 df_ = pd.concat([df_train, df_test])
 #df_.info()
 
 df_["Length_Clean" ] = df_[ "Episode_Length_minutes" ].apply( lambda v: 120.5 if v > 120.5 else v )
+
 df_["IsGuest"] = df_["Guest_Popularity_percentage"].apply( lambda v: 0 if np.isnan(v) else 1 )
-df_["Num_Ads"] = df_["Number_of_Ads"].fillna(0)
+df_["Num_Ads"] = df_["Number_of_Ads"].fillna(0).copy()
 df_["Name_And_Episode"] = df_["Podcast_Name"] + " " + df_["Episode_Title"]
 
-df_["Episode_Completion"] = df_["Listening_Time_minutes"] / df_["Length_Clean"]
-df_['Episode_Completion'] = df_['Episode_Completion'].apply( lambda v: 1 if v > 1 else v )
 
 median_impute = imp.SimpleImputer(missing_values=np.nan, strategy='median', add_indicator=True)
 median_impute.fit(df_[['Length_Clean']])
 length_features = median_impute.transform(df_[['Length_Clean']])
 
 df_['Length_Simple_Impute'] = length_features[:,0]
-df_['Length_Missing'] = length_features[:,1]
+df_['Length_Simple_Missing'] = length_features[:,1]
+
+df_[ TARGET2 ] = df_[TARGET1] / df_["Length_Simple_Impute"]
+df_[ TARGET2 ] = df_[TARGET2].apply( lambda v: 1 if v > 1 else v )
 
 hist, bin_edges = np.histogram( df_["Num_Ads"],
                                 bins=[ df_["Num_Ads"].min(),
@@ -75,35 +77,44 @@ df_["Num_Ads_Bin"] = pd.cut( df_[ "Num_Ads" ], bins=bin_edges,
                              labels=[0, 1, 2, 3], include_lowest=True
                            )
 
-genre_enc = pre.TargetEncoder( cv=3 )
-adsbin_enc = pre.TargetEncoder( cv=3 )
-name_enc  = pre.TargetEncoder( cv=3 )
+
+enc = pre.TargetEncoder( cv=3 )
 
 # prep the cleaned training data to write
-df_train_clean = df_[ df_[ TARGET_COLUMN ].notna() ].copy()
-
-genre_enc.fit( df_train_clean[["Genre"]], df_train_clean[TARGET_COLUMN] )
-df_train_clean["Genre_Encoded"] = genre_enc.transform(df_train_clean[["Genre"]])
-
-adsbin_enc.fit( df_train_clean[["Num_Ads_Bin"]], df_train_clean[TARGET_COLUMN] )
-df_train_clean["Ads_Bin_Encoded"] = adsbin_enc.transform(df_train_clean[["Num_Ads_Bin"]])
-
-name_enc.fit( df_train_clean[["Name_And_Episode"]], df_train_clean[TARGET_COLUMN] )
-df_train_clean["Name_And_Episode_Encoded"] = name_enc.transform(df_train_clean[["Name_And_Episode"]])
-
-#iter_impute = imp.IterativeImputer(missing_values=np.nan, max_iter=20)
-#impute_cols = ["Length_Clean", "Completion_Simple_Impute", "Host_Popularity_percentage",
-#               "Num_Ads_Bin", "Ads_Bin_Encoded", "Genre_Encoded", "Name_And_Episode_Encoded"]
-#iter_impute.fit(df_train_clean[impute_cols])
-#df_train_clean["Length_Iter_Impute" ] = iter_impute.transform(df_train_clean[impute_cols])[:,0]
+df_train_clean = df_[ df_[ TARGET1 ].notna() ].copy()
 
 # prep the cleaned test data to write
-df_test_clean = df_[ df_[ TARGET_COLUMN ].isna() ].copy()
-df_test_clean.drop( TARGET_COLUMN, axis=1, inplace=True )
+df_test_clean = df_[ df_[ TARGET1 ].isna() ].copy()
+df_test_clean.drop( TARGET1, axis=1, inplace=True )
 
-df_test_clean["Genre_Encoded"] = genre_enc.transform(df_test_clean[["Genre"]])
-df_test_clean["Ads_Bin_Encoded"] = adsbin_enc.transform(df_test_clean[["Num_Ads_Bin"]])
-df_test_clean["Name_And_Episode_Encoded"] = name_enc.transform(df_test_clean[["Name_And_Episode"]])
+features = [ "Genre", "Num_Ads_Bin", "Name_And_Episode", "Publication_Day",
+             "Publication_Time", "Podcast_Name", "Episode_Sentiment" ]
+targets = [ TARGET1, TARGET2 ]
+#encoded_features = [ f"T1_{feature}_{targets[0]}", f"T2_{feature}_{targets[1]}" ]
+
+#feature_hash = {
+#    f"T1_{feature}_{targets[0]}": TARGET1,
+#    f"T2_{feature}_{targets[1]}": TARGET2
+#}
+#print(feature_hash)
+
+feature = features[0]
+
+for f in features:
+    i = 1
+    for t in targets:
+        encoded_feature = f"T{i}_{f}_{t}"
+        #print(encoded_feature)
+        enc.fit( df_train_clean[[f]], df_train_clean[t] )
+        df_train_clean[encoded_feature] = enc.transform(df_train_clean[[f]])
+        df_test_clean[encoded_feature] = enc.transform(df_test_clean[[f]])
+        i+=1
+
+#iter_impute = imp.IterativeImputer(missing_values=np.nan, max_iter=20)
+#impute_cols = ["Length_Clean", "Host_Popularity_percentage","Num_Ads_Bin"]
+
+#iter_impute.fit(df_train_clean[impute_cols])
+#df_train_clean["Length_Iter_Impute" ] = iter_impute.transform(df_train_clean[impute_cols])[:,0]
 
 #iter_impute.fit(df_test_clean[impute_cols])
 #df_test_clean["Length_Iter_Impute" ] = iter_impute.transform(df_test_clean[impute_cols])[:,0]
